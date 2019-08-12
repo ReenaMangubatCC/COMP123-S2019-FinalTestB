a010bb0efa89805fef5acfb3ee091f1d2e7f68b1
Reena Mangubat <ReenaM177@outlook.com>
2019-08-12 13:05:52 -0400

    Added data files

---
+++ COMP123-S2019-FinalTestB/Objects/CharacterClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * STUDENT NAME: Reena Mangubat
+ * STUDENT ID: 301000701
+ * DESCRIPTION: This is an enumeration for the Character Class
+ */
+namespace COMP123_S2019_FinalTestB.Objects
+{
+    public enum CharacterClass
+    {
+        FIGHTER,
+        ROGUE,
+        WIZARD,
+        CLERIC,
+        RANGER,
+        MONK,
+        BARBARIAN,
+        PALADIN,
+        BARD,
+        WARLOCK,
+        SORCEROR,
+        DRUID
+    }
+}
